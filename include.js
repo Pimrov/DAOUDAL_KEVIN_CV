750a2f2287a34c5648baa24fc8d8d6ebd1754080
Pimrov <pimrov@gmail.com>
2026-03-17 16:23:37 +0100

    Modif-JS

--- include.js
+++ include.js
@@ -1,32 +1,38 @@
-function includeHTML(id, file) {
-  fetch(file)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Erreur de chargement " + file);
-      }
-      return response.text();
-    })
-    .then(data => {
-      document.getElementById(id).innerHTML = data;
+// function includeHTML(id, file) {
+//   fetch(file)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error("Erreur de chargement " + file);
+//       }
+//       return response.text();
+//     })
+//     .then(data => {
+//       document.getElementById(id).innerHTML = data;
 
-      // if pour HL
-      if (id === "header") {
-        highlightActiveLink();
-      }
-    })
-    .catch(error => console.error(error));
-}
+//       // if pour HL
+//       if (id === "header") {
+//         highlightActiveLink();
+//       }
+//     })
+//     .catch(error => console.error(error));
+// }
 
-includeHTML("header", "header.html");
-includeHTML("banana", "footer.html");
+// includeHTML("header", "header.html");
+// includeHTML("banana", "footer.html");
 
-// Color auto
-function highlightActiveLink() {
-  const currentPage = window.location.pathname.split("/").pop();
+// // Color auto
+// function highlightActiveLink() {
+//   const currentPage = window.location.pathname.split("/").pop();
 
-  document.querySelectorAll("nav a").forEach(link => {
-    if (link.getAttribute("href") === currentPage) {
-      link.classList.add("active");
-    }
-  });
-}
+//   document.querySelectorAll("nav a").forEach(link => {
+//     if (link.getAttribute("href") === currentPage) {
+//       link.classList.add("active");
+//     }
+//   });
+// }
+
+const inc = (id, f) => fetch(f).then(r => r.ok ? r.text() : Promise.reject(f)).then(h => document.getElementById(id).innerHTML = h);
+
+Promise.all([inc("header","header.html"), inc("banana","footer.html")])
+  .then(() => { const p = location.pathname.split("/").pop(); document.querySelectorAll("nav a").forEach(a => a.getAttribute("href") === p && a.classList.add("active")); })
+  .catch(console.error);
